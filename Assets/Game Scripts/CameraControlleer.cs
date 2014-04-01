cdeb97f83f2f2ba60363f63f7aeae61012d896d4
kristina <kristina.fernandes7@gmail.com>
2014-04-01 21:43:32 +0100

    Submit

---
+++ Assets/Game Scripts/CameraControlleer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControlleer : MonoBehaviour 
+{
+
+	public GameObject Player;
+	private Vector3 offset;
+	
+	void Start ()
+	{
+		offset = transform.position;
+	}
+	
+	void LateUpdate ()
+	{
+		transform.position = Player.transform.position + offset;
+	}
+}
---
+++ Assets/Game Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+	
+	
+	void Start()
+	{
+		count = 0;
+		SetCountText();
+		winText.text = "";
+	}
+	
+	void FixedUpdate () 
+	{
+		 
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
+			
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}	
+		// destroy game object when touched : Destroy(other.gameObject);
+		
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "PickUp")
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText();
+		}
+	}
+	 void SetCountText()
+	 {
+		countText.text = "Count: " + count.ToString();
+		if (count >= 7)
+		{
+			winText.text = " CONGRATULATIONS! You Win ";
+		}
+	 }
+		
+}
